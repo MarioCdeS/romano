72ab6f4cec43e1a22c9220c171ab7ee6fb4e0254
Mario de Sousa <desousa.mc@gmail.com>
2018-12-03 16:55:56 +0200

    EoS

--- main.go
+++ main.go
@@ -3,26 +3,52 @@ package main
 import (
 	"fmt"
 	"image/png"
-	"math"
 	"os"
 
+	"github.com/MarioCdeS/romano/tracer/geometry"
 	"github.com/MarioCdeS/romano/tracer/graphics"
 	"github.com/MarioCdeS/romano/tracer/linalg"
 )
 
 func main() {
-	orig := linalg.Point{400, 300, 0}
-	rotZ := linalg.NewRotateZ(math.Pi / 6)
-	white := graphics.Color{1, 1, 1, 1}
-	tick := linalg.Vector{200, 0, 0}
-
 	cnvs := graphics.NewCanvas(800, 600)
-	cnvs.Clear(graphics.Color{0, 0, 0, 1})
+	red := graphics.Color{1, 0, 0, 1}
+	black := graphics.Color{0, 0, 0, 1}
 
-	for i := 0; i < 12; i++ {
-		tickPoint := orig.AddVector(tick)
-		cnvs.Set(int(tickPoint.X), int(tickPoint.Y), white)
-		tick = rotZ.DotVector(tick)
+	cam := linalg.Point{4, 3, -5}
+	orig := linalg.Point{0, 0, 0}
+	xAxis := linalg.Vector{8, 0, 0}
+	yAxis := linalg.Vector{0, 6, 0}
+	xTick := 1 / float64(cnvs.Width())
+	yTick := 1 / float64(cnvs.Height())
+
+	sphere, err := geometry.NewTransformedSphere(linalg.NewTranslate(4, 3, 0).Dot(linalg.NewScale(3, 3, 3)))
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for y := 0; y < cnvs.Height(); y++ {
+		for x := 0; x < cnvs.Width(); x++ {
+			// We are not worried about the distance to the hit, just that a hit occurred, so rayDir doesn't need to be
+			// normalised.
+			origCopy := orig
+			rayDir := origCopy.
+				MutAddVector(xAxis.Scale(float64(x) * xTick)).
+				MutAddVector(yAxis.Scale(float64(y) * yTick)).
+				SubPoint(cam)
+			ray := geometry.Ray{Origin: cam, Direction: rayDir}
+			var col graphics.Color
+
+			if _, ok := geometry.Hit(sphere.Intersections(&ray)); ok {
+				col = red
+			} else {
+				col = black
+			}
+
+			cnvs.Set(x, cnvs.Height()-y-1, col)
+		}
 	}
 
 	f, err := os.Create("out.png")
